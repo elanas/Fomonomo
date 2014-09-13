f1d324b4a9ee0926e1921dc57046068a4ad07b10
Elana Stroud <estroud1@jhu.edu>
2014-09-13 03:32:14 -0400

    basic scaffolding for poster and trailer divs

---
+++ public/javascripts/webapp.js
@@ -0,0 +1,15 @@
+/**
+ * Created by elanastroud on 9/13/14.
+ */
+
+(function () {
+
+    var app = angular.module("webapp", []);
+
+    app.controller('webController', ['$window', '$scope', '$http', '$timeout', function ($window, $scope, $http, $timeout) {
+
+
+    }]);
+
+})();
+
